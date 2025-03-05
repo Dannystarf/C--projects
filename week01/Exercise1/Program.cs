fd991e4c57b15c620f00de969652cc93c5c5bbd1
dannystarf <danielajayi1012042004@gmail.com>
2025-03-05 09:36:33 -0800

    chnage my varaible naming convention to camel case

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,17 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string fname = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
-        fname = char.ToUpper(fname[0]) + fname.Substring(1).ToLower();
+        firstName = char.ToUpper(firstName[0]) + firstName.Substring(1).ToLower();
 
         Console.Write("What is your last name? ");
-        string lname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        lname = char.ToUpper(lname[0]) + lname.Substring(1).ToLower();
+        lastName = char.ToUpper(lastName[0]) + lastName.Substring(1).ToLower();
 
         Console.WriteLine("");
 
-        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
