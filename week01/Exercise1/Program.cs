79e34abfa987b605652eca59b14f9d9e751cb9a5
dannystarf <danielajayi1012042004@gmail.com>
2025-03-05 09:20:16 -0800

    Excercise 1 project

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        fname = char.ToUpper(fname[0]) + fname.Substring(1).ToLower();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        lname = char.ToUpper(lname[0]) + lname.Substring(1).ToLower();
+
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
